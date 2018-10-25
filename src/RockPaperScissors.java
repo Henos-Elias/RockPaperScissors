66c7747c1e4ae84be28d39df4687c855f9f66901
Henos Elias <henos.elias.2021@student.lhschools.org>
2018-10-25 08:58:11 -0400

    intelij

---
+++ src/RockPaperScissors.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class RockPaperScissors {
+
+    public static void main(String[] args) {
+        int input;
+        int b=1;
+        Scanner sage= new Scanner(System.in);
+        Random rnd = new Random();
+        System.out.println("Rock Paper Scissors");
+        System.out.println("Select 1, 2, 3, for rock, paper, scissors");
+    }
+}
